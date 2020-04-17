8a11e05e0463ecdb0762d56c04aebbef46ff23a6
asamokhina94 <cdfiY#16Z>
2020-04-17 17:04:48 +0300

    code changed

--- src/Task_6.java
+++ src/Task_6.java
@@ -4,14 +4,14 @@ public class Task_6 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.print("Введите x: ");
-        int x = in.nextInt();
+        double x = in.nextDouble();
         System.out.print("Введите y: ");
-        int y = in.nextInt();
+        double y = in.nextDouble();
         System.out.print("Введите z: ");
-        int z = in.nextInt();
-        int num = (x + y + z) / 2; // Запись в переменную среднего арифметического
+        double z = in.nextDouble();
+        double num = (x + y + z) / 3; // Запись в переменную среднего арифметического
         System.out.println("Среднее арифметическое чисел x,y,z: " + num);
-        int del = num / 2; // деление среднего арифметического на 2 без остатка
+        int del = (int)num / 2; // деление среднего арифметического на 2 без остатка
         if (del > 3) {
             System.out.println("Программа выполнена корректно");
         }
